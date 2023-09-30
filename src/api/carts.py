518afa2a8d5a460649dac0dacda934d4ac27824b
iloo <igloo405@gmail.com>
2023-09-29 17:31:48 -0700

    fix cart

--- src/api/carts.py
+++ src/api/carts.py
@@ -24,12 +24,12 @@ def create_cart(new_cart: NewCart):
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
     """ """    
-    return {
+    return [
             {
                 "potion_type": [100, 0, 0, 0],
                 "quantity": 0,
             }
-        }
+        ]
 
 
 class CartItem(BaseModel):
@@ -57,9 +57,21 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
     #only allowing customers to buy one potion right now
     with db.engine.begin() as connection: 
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory WHERE id=1"))
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, gold FROM global_inventory WHERE id=1"))
         data = result.fetchone()
-        if(data[0] > 0):
-            return {"total_potions_bought": 1, "total_gold_paid": 50}
+        num_red_potions = data[0]
+        gold = data[1]
+        #if there's stock
+        if(num_red_potions > 0):
+            #check if customer paid enough
+            if(cart_checkout.payment >= 50):
+                #add to gold and subtract from potion stock 
+                new_num_red_potions = num_red_potions - 1
+                new_gold = gold + cart_checkout.payment
+                
+                connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=:goldset, num_red_potions=:potionset"),{'goldset':new_gold}, {'potionset':new_num_red_potions})
+                return {"total_potions_bought": 1, "total_gold_paid": 50}
+            else: 
+                return "INSUFFICIENT PAYMENT"
         else: 
             return "OUT OF STOCK"
