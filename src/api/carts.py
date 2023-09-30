616bd48ca0c9bcb89be800458e503892ed393492
iloo <igloo405@gmail.com>
2023-09-29 17:08:31 -0700

    cart

--- src/api/carts.py
+++ src/api/carts.py
@@ -1,5 +1,5 @@
-# import sqlalchemy
-# from src import database as db
+import sqlalchemy
+from src import database as db
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
@@ -18,15 +18,18 @@ class NewCart(BaseModel):
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
-    i = i + 1
-    return {"cart_id": i}
+    return {"cart_id": 1}
 
 
 @router.get("/{cart_id}")
 def get_cart(cart_id: int):
-    """ """
-
-    return {}
+    """ """    
+    return {
+            {
+                "potion_type": [100, 0, 0, 0],
+                "quantity": 0,
+            }
+        }
 
 
 class CartItem(BaseModel):
@@ -36,8 +39,14 @@ class CartItem(BaseModel):
 @router.post("/{cart_id}/items/{item_sku}")
 def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     """ """
+    with db.engine.begin() as connection: 
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory WHERE id=1"))
+        data = result.fetchone()
 
-    return "OK"
+        if(data[0] >= cart_item.quantity and item_sku == "RED POTION"):
+            return "OK"
+        else:
+            return "QUANTITY TOO HIGH"
 
 
 class CartCheckout(BaseModel):
@@ -46,7 +55,11 @@ class CartCheckout(BaseModel):
 @router.post("/{cart_id}/checkout")
 def checkout(cart_id: int, cart_checkout: CartCheckout):
     """ """
-
-    return {"total_potions_bought": 1, "total_gold_paid": 50}
-# with db.engine.begin() as connection:
-#         result = connection.execute(sql_to_execute)
+    #only allowing customers to buy one potion right now
+    with db.engine.begin() as connection: 
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory WHERE id=1"))
+        data = result.fetchone()
+        if(data[0] > 0):
+            return {"total_potions_bought": 1, "total_gold_paid": 50}
+        else: 
+            return "OUT OF STOCK"
