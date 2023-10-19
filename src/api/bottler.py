2d24e8e3db0b59e69e913656be9292b76fc1dfac
iloo <igloo405@gmail.com>
2023-10-19 14:42:13 -0700

    bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -1,3 +1,4 @@
+import random
 import sqlalchemy
 from src import database as db
 from fastapi import APIRouter, Depends
@@ -20,28 +21,6 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     with db.engine.begin() as connection: 
         print(potions_delivered)
-        # result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml FROM global_inventory WHERE id=1"))
-        #get all the qty of potions we have
-        # red = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=1"))
-        # green = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=2"))
-        # blue = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=4"))
-        # purple = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=3"))
-        # red = red.fetchone()
-        # red = red[0]
-        # green = green.fetchone()
-        # green = green[0]
-        # purple = purple.fetchone()
-        # purple = purple[0]
-        # blue = blue.fetchone()
-        # blue = blue[0]
-
-        # data = result.fetchone()
-        # # num_red_potions = data[0]
-        # num_red_ml = data[0]
-        # # num_green_potions = data[2]
-        # num_green_ml = data[1]
-        # # num_blue_potions = data[4]
-        # num_blue_ml = data[2]
         rows = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
         rows = rows.fetchall()
 
@@ -54,82 +33,6 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
                     connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml= num_red_ml - :rowred, num_green_ml = num_green_ml - :rowgreen, num_blue_ml = num_blue_ml - :rowblue"), {'rowred':row.red * potion.quantity, 'rowgreen':row.green * potion.quantity, 'rowblue':row.blue * potion.quantity})
                     #UPDATE THE ROW THROUGH THE SKU WITH QTY
                     #UPDATE THE GLOBAL INVENTORY 
-
-            #if we have a red potion add to num_red_potions
-            #also subtract from num red ml when bottling potions 
-
-
-
-            # if potion.potion_type[0] == 0 and potion.potion_type[1] == 50 and potion.potion_type[2] == 50:
-            #     #seahawks
-            #     seahawks += potion.quantity
-            #     num_green_ml -= potion.quantity * 50
-            #     num_blue_ml -= potion.quantity * 50
-            # if potion.potion_type[0] == 50 and potion.potion_type[1] == 50 and potion.potion_type[2] == 0: 
-            #     #christmas
-            #     christmas += potion.quantity
-            #     num_green_ml -= potion.quantity * 50
-            #     num_red_ml -= potion.quantity * 50
-            # if potion.potion_type[0] == 50 and potion.potion_type[1] == 0 and potion.potion_type[2] == 50:
-            #     #purple
-            #     purple += potion.quantity
-            #     num_red_ml -= potion.quantity * 50
-            #     num_blue_ml -= potion.quantity * 50
-
-
-
-            # if potion.potion_type[0] == 100:
-            #     num_red_potions += potion.quantity
-            #     num_red_ml -= potion.quantity * 100
-            # elif potion.potion_type[1] == 100:
-            #     num_green_potions += potion.quantity
-            #     num_green_ml -= potion.quantity * 100
-            # elif potion.potion_type[2] == 100: 
-            #     num_blue_potions += potion.quantity
-            #     num_blue_ml -= potion.quantity * 100
-        
-        # value = {'red_potions':num_red_potions}
-        # value2 = {'red_ml':num_red_ml}
-        # value3 = {'green_potions': num_green_potions}
-        # value4 = {'green_ml':num_green_ml}
-        # value5 = {'blue_potions': num_blue_potions}
-        # value6 = {'blue_ml': num_blue_ml}
-
-        # value = {'seahawks_amt':seahawks}
-        # value2 = {'christmas_amt':christmas}
-        # value3 = {'purple_amt':purple}
-        # value4 = {'red_ml':num_red_ml}
-        # value5 = {'green_ml':num_green_ml}
-        # value6 = {'blue_ml': num_blue_ml}
-
-        # sql = sqlalchemy.text("UPDATE potions SET quantity=:seahawks_amt WHERE id=1")
-        # sql2 = sqlalchemy.text("UPDATE potions SET quantity=:christmas_amt WHERE id=2")
-        # sql3 = sqlalchemy.text("UPDATE potions SET quantity=:purple_amt WHERE id=3")
-        # sql4 = sqlalchemy.text("UPDATE global_inventory SET num_red_ml=:red_ml")
-        # sql5 = sqlalchemy.text("UPDATE global_inventory SET num_green_ml=:green_ml")
-        # sql6 = sqlalchemy.text("UPDATE global_inventory SET num_blue_ml=:blue_ml")
-        # connection.execute(sql, value)
-        # connection.execute(sql2, value2)
-        # connection.execute(sql3, value3)
-        # connection.execute(sql4, value4)
-        # connection.execute(sql5, value5)
-        # connection.execute(sql6, value6)
-        # sql = sqlalchemy.text("UPDATE global_inventory SET num_red_potions=:red_potions",)
-        # sql2 = sqlalchemy.text("UPDATE global_inventory SET num_red_ml=:red_ml")
-        # connection.execute(sql, value)
-        # connection.execute(sql2, value2)
-
-        # sql3 = sqlalchemy.text("UPDATE global_inventory SET num_green_potions=:green_potions",)
-        # sql4 = sqlalchemy.text("UPDATE global_inventory SET num_green_ml=:green_ml") 
-        # connection.execute(sql3, value3)
-        # connection.execute(sql4, value4)
-
-        # sql5 = sqlalchemy.text("UPDATE global_inventory SET num_blue_potions=:blue_potions",)
-        # sql6 = sqlalchemy.text("UPDATE global_inventory SET num_blue_ml=:blue_ml") 
-        # connection.execute(sql5, value5)
-        # connection.execute(sql6, value6)
-
-
         print(potions_delivered)
         return "OK"
 
@@ -143,86 +46,62 @@ def get_bottle_plan():
     # Each bottle has a quantity of what proportion of red, blue, and
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
-
-    # Initial logic: bottle all barrels into red potions.
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml FROM global_inventory WHERE id=1"))
-        red = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=1"))
-        green = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=2"))
-        purple = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=3"))
-        blue = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=4"))
+        # apparently this is hard coded
+        potion_inventory = connection.execute(sqlalchemy.text("SELECT red, green, blue, quantity FROM potions"))
+        potion_inventory = potion_inventory.fetchall()
+
+        potion_list = []
+        for row in potion_inventory: 
+            potion_list.append(([row.red, row.green, row.blue, 0], row.quantity))
+        print("potion_list")
+        print(potion_list)
+
         data = result.fetchone()
         num_red_ml = data[0]
         num_green_ml = data[1]
         num_blue_ml = data[2]
-        red = red.fetchone()
-        red = red[0]
-        green = green.fetchone()
-        green = green[0]
-        purple = purple.fetchone()
-        purple = purple[0]
-        blue = blue.fetchone()
-        blue = blue[0]
-
-        potions = [("PURPLE", purple), ("RED", red), ("GREEN", green), ("BLUE", blue)]
-        potions = sorted(potions, key=lambda x: x[1])
+        random.shuffle(potion_list)
+        #sort the potion_list by qty
+        potion_list = sorted(potion_list, key=lambda x: x[1])
         print("new potions sorted")
-        print(potions)
-
-        red_qty = (num_red_ml // 100) // 2
-        green_qty = (num_green_ml // 100) // 2
-        blue_qty = (num_blue_ml // 100) // 2 
-        purple_qty = min(num_red_ml // 100, num_blue_ml // 100)
-
-        total = red_qty + green_qty + blue_qty + purple_qty
-
-        print("red quantity: " + str(red_qty))
-        print("green quantity: " + str(green_qty))
-        print("blue quantity: " + str(blue_qty))
-        print("purple quantity: " + str(purple_qty))
+        print(potion_list)
         result = []
-        potionsQty = dict([("PURPLE", purple_qty), ("RED", red_qty), ("GREEN", green_qty), ("BLUE", blue_qty)])
-
-
-        #i should probably add a num_sold col to try and make sure I sell equal amt
-        #loop through in order of potions i need to make
-
-        #means we have qty to bottle
-        if total > 0: 
-            potions = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
-            potions = potions.fetchall()
-            for pot in potions: 
-                if potionsQty[pot.sku] > 0: 
-                    result.append({
-                        "potion_type": [pot.red, pot.green, pot.blue, pot.dark],
-                        "quantity": potionsQty[pot.sku],
-                    })
-
-        # for potion in potions: 
-        #     pot_type = potion[0]
-        #     if pot_type == "red":
-        #         if red_qty > 0:
-        #             result.append({
-        #                         "potion_type": [100, 0, 0, 0],
-        #                         "quantity": red_qty,
-        #                     })
-        #     if pot_type == "green":
-        #         if green_qty > 0: 
-        #             result.append({
-        #                         "potion_type": [0, 100, 0, 0],
-        #                         "quantity": green_qty,
-        #                     })
-        #     if pot_type == "blue":
-        #         if blue_qty > 0: 
-        #             result.append({
-        #                         "potion_type": [0, 0, 100, 0],
-        #                         "quantity": blue_qty,
-        #                     })
-        #     if pot_type == "purple":
-        #         if purple_qty > 0:
-        #             result.append({
-        #                         "potion_type": [50, 0, 50, 0],
-        #                         "quantity": purple_qty,
-        #                     })
+        qty_list = []
+        red_qty = 0
+        blue_qty = 0
+        green_qty = 0
+        for potion in potion_list: 
+            pot_type = potion[0]
+            qty_list = []
+            if pot_type[0] <= num_red_ml and pot_type[1] <= num_green_ml and pot_type[2] <= num_blue_ml: 
+                print("at top we have")
+                print("red " + str(num_red_ml))
+                print("blue " + str(num_blue_ml))
+                print("green " + str(num_green_ml))
+                if pot_type[0] > 0: 
+                    #gives me how much of this pot type i can make
+                    red_qty = num_red_ml // pot_type[0]
+                    qty_list.append(red_qty)
+                    num_red_ml -= red_qty * pot_type[0]
+                    print("remaining red ml is " + str(num_red_ml))
+                if pot_type[1] > 0: 
+                    green_qty = num_green_ml // pot_type[1]
+                    qty_list.append(green_qty)
+                    num_green_ml -= green_qty * pot_type[1]
+                if pot_type[2] > 0: 
+                    blue_qty = num_blue_ml // pot_type[2]
+                    qty_list.append(blue_qty)
+                    num_blue_ml -= blue_qty * pot_type[2]
+            if len(qty_list) > 0:
+                result.append({
+                    "potion_type": [pot_type[0], pot_type[1], pot_type[2], pot_type[3]],
+                    "quantity": min(qty_list)
+                })
+                
+        print("calculated qty list")
+        print(qty_list)
+        print(result)
                     
         return result
--- src/api/carts.py
+++ src/api/carts.py
@@ -14,23 +14,16 @@ router = APIRouter(
 class NewCart(BaseModel):
     customer: str
 
-cart_memory = {}
 
-cartID = 0
 @router.post("/")
 def create_cart(new_cart: NewCart):
     """ """
     print("customer:")
     print(new_cart.customer)
-    global cartID
-    cartID += 1
-    cart_memory[cartID] = []
     #insert row for new cart
     value = {'name':new_cart.customer}
     with db.engine.begin() as connection: 
-        # sql = sqlalchemy.text("INSERT INTO carts (customer) VALUES (:value) RETURNING id")
-        result = connection.execute(sqlalchemy.text("INSERT INTO carts (customer) VALUES (:name) RETURNING id"), [{"name":new_cart.customer}])
-        # result = connection.execute(sql, value)
+        result = connection.execute(sqlalchemy.text("INSERT INTO carts (customer) VALUES (:name) RETURNING id"), [{'name':new_cart.customer}])
     print("cartID")
     result = result.fetchone()
     print(result[0])
@@ -42,8 +35,11 @@ def get_cart(cart_id: int):
     """ """  
     # with db.engine.begin() as connection: 
     #     result = connection.execute(sqlalchemy.text("SELECT "))
-
-    return cart_memory.get(cart_id)
+    # with db.engine.begin() as connection: 
+    #     result = connection.execute(sqlalchemy.text("SELECT * FROM cart_items WHERE cart_id=:id"), {"id": cart_id})
+        
+    return 
+    # return cart_memory.get(cart_id)
     # return [
             # {
             #     "potion_type": [100, 0, 0, 0],
@@ -62,35 +58,14 @@ def set_item_quantity(cart_id: int, item_sku: str, cart_item: CartItem):
     with db.engine.begin() as connection: 
         potionID = connection.execute(sqlalchemy.text("SELECT * FROM potions WHERE sku=:itemsku"), {'itemsku':item_sku})
         potionID = potionID.fetchone()
-        # potionID = potionID.id
         print("potionID ->  " + str(potionID.id))
         print("qty:")
         print(cart_item.quantity)
-        if cart_item.quantity <= potionID.quantity:
+        if cart_item.quantity < potionID.quantity:
             connection.execute(sqlalchemy.text("INSERT INTO cart_items (potion_id, cart_id, quantity) VALUES (:potID, :cartID, :qty)"), {'potID':potionID.id, 'cartID':cart_id, 'qty':cart_item.quantity})
             return "OK"
         else: 
             return "QUANTITY TOO HIGH"
-
-        # if item_sku == "RED_POTION":
-        #     if num_red_potions >= cart_item.quantity:
-        #         # cart_memory[cartID] = ([{"potion_type": [100, 0, 0, 0], "quantity": cart_item.quantity}])
-        #         cart_memory[cart_id].append({"potion_type": [100, 0, 0, 0], "quantity": cart_item.quantity})
-        #         return "OK"
-        #     else:
-        #         return "QUANTITY TOO HIGH"
-        # elif item_sku == "GREEN_POTION":
-        #     if num_green_potions >= cart_item.quantity:
-        #         # cart_memory[cartID] = {"potion_type": [0, 100, 0, 0], "quantity": cart_item.quantity}
-        #         cart_memory[cart_id].append({"potion_type": [0, 100, 0, 0], "quantity": cart_item.quantity})
-        #     else:
-        #         return "QUANTITY TOO HIGH"
-        # elif item_sku == "BLUE_POTION":
-        #     if num_blue_potions >= cart_item.quantity:
-        #         # cart_memory[cartID] = {"potion_type": [0, 0, 100, 0], "quantity": cart_item.quantity}
-        #         cart_memory[cart_id].append({"potion_type": [0, 0, 100, 0], "quantity": cart_item.quantity})
-        #     else:
-        #         return "QUANTITY TOO HIGH"
 
 class CartCheckout(BaseModel):
     payment: str
@@ -102,23 +77,11 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     print(cart_checkout.payment)
 
     with db.engine.begin() as connection: 
-        # result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions, gold FROM global_inventory WHERE id=1"))
-        # data = result.fetchone()
-        # num_red_potions = data[0]
-        # num_green_potions = data[1]
-        # num_blue_potions = data[2]
-        # gold = data[3]
-        # cart = cart_memory[cart_id]
-        # print(cart)
-        # potion_type = cart["potion_type"]
         total_potions = 0
-        #red
         cartItems = connection.execute(sqlalchemy.text("SELECT * FROM cart_items WHERE cart_id=:cartID"), {'cartID':cart_id})
         cartItems = cartItems.fetchall()
 
         for item in cartItems:
-            # potion_type = item_list["potion_type"]
-            # qty = item_list["quantity"]
             potionID = item.potion_id
             qty = item.quantity
             print("potion ID")
@@ -137,34 +100,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             total_potions += qty
             #increment gold
             connection.execute(sqlalchemy.text("UPDATE global_inventory set gold= gold + :price"), {'price':qty * 50})
-
-            # if potion_type[0] == 100:
-            #     num_red_potions -= qty
-            #     total_potions +=qty
-            #     gold += 50 * qty
-            #     connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=:goldset, num_red_potions=:red_potion"), {'goldset':gold, 'red_potion': num_red_potions})
-            # if potion_type[1] == 100: 
-            #     num_green_potions -= qty
-            #     total_potions +=qty
-            #     gold += 50 * qty
-            #     connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=:goldset, num_green_potions=:green_potion"), {'goldset':gold, 'green_potion': num_green_potions})
-            # if potion_type[2] == 100: 
-            #     num_blue_potions -= qty
-            #     total_potions +=qty
-            #     gold += 50 * qty
-            #     connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=:goldset, num_blue_potions=:blue_potion"), {'goldset':gold, 'blue_potion': num_blue_potions})
-        #if there's stock
-
-        # print("total gold is: " + str(gold))
         print("total gold paid is: "  + str(total_potions * 50))
-
-        # if(num_red_potions > 0):
-        #     #check if customer paid enough
-        #     #add to gold and subtract from potion stock 
-        #     new_num_red_potions = num_red_potions - 1
-        #     new_gold = gold + 50
-        #     connection.execute(sqlalchemy.text("UPDATE global_inventory SET gold=:goldset, num_red_potions=:potionset"),{'goldset':new_gold,'potionset':new_num_red_potions})
-            
+         
         return {"total_potions_bought": total_potions, "total_gold_paid": total_potions*50}
-        # else: 
-        #     return "OUT OF STOCK"
