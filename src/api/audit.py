a4215f8f02e874085b94ce96fb232571fedf0769
iloo <igloo405@gmail.com>
2023-10-21 15:54:55 -0700

    implemented audit

--- src/api/audit.py
+++ src/api/audit.py
@@ -15,24 +15,42 @@ router = APIRouter(
 def get_inventory():
     """ """
     with db.engine.begin() as connection: 
-        result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, gold FROM global_inventory WHERE id=1"))
-        data = result.fetchone()
-        total_ml = data[0] + data[1] + data[2]
-        gold = data[3]
-        potion1 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=1"))
-        potion2 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=2"))
-        potion3 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=3"))
-        potion4 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=4"))
-        potion1 = potion1.fetchone()
-        potion1 = potion1[0]
-        potion2 = potion2.fetchone()
-        potion2 = potion2[0]
-        potion3 = potion3.fetchone()
-        potion3 = potion3[0]
-        potion4 = potion4.fetchone()
-        potion4 = potion4[0]
+        # result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_green_ml, num_blue_ml, gold FROM global_inventory WHERE id=1"))
+        # data = result.fetchone()
+        # total_ml = data[0] + data[1] + data[2]
+        # gold = data[3]
+        total_potions = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM potion_ledger"))
+        total_potions = total_potions.fetchone()
+        total_potions = total_potions.balance
+        red_ml = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM red_ml_ledger"))
+        red_ml = red_ml.fetchone()
+        red_ml = red_ml.balance
 
-        return {"number_of_potions": potion1 + potion2 + potion3 + potion4, "ml_in_barrels": total_ml, "gold": gold}
+        blue_ml = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM blue_ml_ledger"))
+        blue_ml = blue_ml.fetchone()
+        blue_ml = blue_ml.balance
+
+        green_ml = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM green_ml_ledger"))
+        green_ml = green_ml.fetchone()
+        green_ml = green_ml.balance
+
+        gold = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM gold_ledger"))
+        gold = gold.fetchone()
+        gold = gold.balance
+        # potion1 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=1"))
+        # potion2 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=2"))
+        # potion3 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=3"))
+        # potion4 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=4"))
+        # potion1 = potion1.fetchone()
+        # potion1 = potion1[0]
+        # potion2 = potion2.fetchone()
+        # potion2 = potion2[0]
+        # potion3 = potion3.fetchone()
+        # potion3 = potion3[0]
+        # potion4 = potion4.fetchone()
+        # potion4 = potion4[0]
+
+        return {"number_of_potions": total_potions, "ml_in_barrels": red_ml + green_ml + blue_ml, "gold": gold}
 
 class Result(BaseModel):
     gold_match: bool
