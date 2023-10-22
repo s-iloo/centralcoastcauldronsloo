1236fbd0cda977ee043cf011c10f9eae8c776b91
iloo <igloo405@gmail.com>
2023-10-22 15:30:51 -0700

    added comma

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -31,7 +31,7 @@ def get_catalog():
                 "name": row.name,
                 "quantity": quantity,
                 "price": row.price,
-                "potion_type": [row.red, row.green, row.blue, row.dark]
+                "potion_type": [row.red, row.green, row.blue, row.dark],
             })
         
         return result
