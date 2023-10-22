f0f2ac396298ed59b4044dda2745802c763520c3
iloo <igloo405@gmail.com>
2023-10-22 14:47:08 -0700

    try now

--- src/api/audit.py
+++ src/api/audit.py
@@ -29,27 +29,14 @@ def get_inventory():
         blue_ml = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM blue_ml_ledger"))
         blue_ml = blue_ml.fetchone()
         blue_ml = blue_ml.balance
-
+        print(blue_ml)
         green_ml = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM green_ml_ledger"))
         green_ml = green_ml.fetchone()
         green_ml = green_ml.balance
-
+        print(green_ml)
         gold = connection.execute(sqlalchemy.text("SELECT SUM(change) AS balance FROM gold_ledger"))
         gold = gold.fetchone()
         gold = gold.balance
-        # potion1 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=1"))
-        # potion2 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=2"))
-        # potion3 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=3"))
-        # potion4 = connection.execute(sqlalchemy.text("SELECT quantity FROM potions WHERE id=4"))
-        # potion1 = potion1.fetchone()
-        # potion1 = potion1[0]
-        # potion2 = potion2.fetchone()
-        # potion2 = potion2[0]
-        # potion3 = potion3.fetchone()
-        # potion3 = potion3[0]
-        # potion4 = potion4.fetchone()
-        # potion4 = potion4[0]
-
         return {"number_of_potions": total_potions, "ml_in_barrels": red_ml + green_ml + blue_ml, "gold": gold}
 
 class Result(BaseModel):
