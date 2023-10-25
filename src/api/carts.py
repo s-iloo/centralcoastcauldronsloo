609f3d0920299416058e3e7fae81e58c0ea7fb91
iloo <igloo405@gmail.com>
2023-10-25 12:50:00 -0700

    changed result statement

--- src/api/carts.py
+++ src/api/carts.py
@@ -3,12 +3,70 @@ from src import database as db
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
 from src.api import auth
+from enum import Enum
 
 router = APIRouter(
     prefix="/carts",
     tags=["cart"],
     dependencies=[Depends(auth.get_api_key)],
 )
+
+class search_sort_options(str, Enum):
+    customer_name = "customer_name"
+    item_sku = "item_sku"
+    line_item_total = "line_item_total"
+    timestamp = "timestamp"
+
+class search_sort_order(str, Enum):
+    asc = "asc"
+    desc = "desc"   
+
+@router.get("/search/", tags=["search"])
+def search_orders(
+    customer_name: str = "",
+    potion_sku: str = "",
+    search_page: str = "",
+    sort_col: search_sort_options = search_sort_options.timestamp,
+    sort_order: search_sort_order = search_sort_order.desc,
+):
+    """
+    Search for cart line items by customer name and/or potion sku.
+
+    Customer name and potion sku filter to orders that contain the 
+    string (case insensitive). If the filters aren't provided, no
+    filtering occurs on the respective search term.
+
+    Search page is a cursor for pagination. The response to this
+    search endpoint will return previous or next if there is a
+    previous or next page of results available. The token passed
+    in that search response can be passed in the next search request
+    as search page to get that page of results.
+
+    Sort col is which column to sort by and sort order is the direction
+    of the search. They default to searching by timestamp of the order
+    in descending order.
+
+    The response itself contains a previous and next page token (if
+    such pages exist) and the results as an array of line items. Each
+    line item contains the line item id (must be unique), item sku, 
+    customer name, line item total (in gold), and timestamp of the order.
+    Your results must be paginated, the max results you can return at any
+    time is 5 total line items.
+    """
+
+    return {
+        "previous": "",
+        "next": "",
+        "results": [
+            {
+                "line_item_id": 1,
+                "item_sku": "1 oblivion potion",
+                "customer_name": "Scaramouche",
+                "line_item_total": 50,
+                "timestamp": "2021-01-01T00:00:00Z",
+            }
+        ],
+    }
 
 
 class NewCart(BaseModel):
@@ -80,13 +138,17 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     print(cart_checkout.payment)
     total_potions = 0
     with db.engine.begin() as connection: 
-        
-        cartItems = connection.execute(sqlalchemy.text("SELECT * FROM cart_items WHERE cart_id=:cartID"), {'cartID':cart_id})
+        result = connection.execute(sqlalchemy.text("SELECT cart_items.cart_id, cart_items.quantity, cart_items.potion_id, potions.price FROM cart_items LEFT JOIN potions ON cart_items.potion_id = potions.id WHERE cart_id = :cart_id"), 
+                                    {"cart_id": cart_id})
+        # cartItems = connection.execute(sqlalchemy.text("SELECT * FROM cart_items WHERE cart_id=:cartID"), {'cartID':cart_id})
         # cartItems = cartItems.fetchall()
-        for item in cartItems:
+        for item in result:
             potionID = item.potion_id
             qty = item.quantity
             total_potions += item.quantity
+            payment = item.price * item.quantity
+            total_price += payment
+            
             print("current total_potions is: ")
             print(total_potions)
             print("potion ID")
@@ -99,9 +161,9 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             print("potion sku")
             print(potionType.sku)
             print("you're buying " + potionType.sku)
-            connection.execute(sqlalchemy.text("INSERT INTO potion_ledger (change, potion_id) VALUES (:ch, :potID)"), {'ch':-item.quantity, 'potID': potionID})                
+            connection.execute(sqlalchemy.text("INSERT INTO potion_ledger (change, potion_id) VALUES (:ch, :potID)"), {'ch':-(item.quantity), 'potID': potionID})                
             
-            ledgeID = connection.execute(sqlalchemy.text("INSERT INTO gold_ledger (change) VALUES (:ch) RETURNING id"), {'ch':30 * qty})
+            ledgeID = connection.execute(sqlalchemy.text("INSERT INTO gold_ledger (change) VALUES (:ch) RETURNING id"), {'ch':payment})
             ledgeID = ledgeID.fetchone()
             ledgeID = ledgeID.id
 
@@ -110,4 +172,4 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
             connection.execute(sqlalchemy.text("INSERT INTO transactions (description) VALUES (:desc)"), {'desc': desc})
 
     print("total gold paid is: "  + str(total_potions * 30))
-    return {"total_potions_bought": total_potions, "total_gold_paid": total_potions*30}
+    return {"total_potions_bought": total_potions, "total_gold_paid": total_price}
