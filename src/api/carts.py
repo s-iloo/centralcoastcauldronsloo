9f63ac5aa245268ec023c169a2336ed057ac7657
iloo <igloo405@gmail.com>
2023-10-27 15:47:42 -0700

    loop

--- src/api/carts.py
+++ src/api/carts.py
@@ -91,7 +91,7 @@ def search_orders(
         returned = []
         # i = int(search_page)
         i = 0
-        for item in range(n, n + 4): 
+        for item in range(n + 1, n + 5): 
             i += 1
             print(i)
 
